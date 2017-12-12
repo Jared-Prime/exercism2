fb706d25d5d28f2ec642369d6995a45ff2ee43dc
Jared Davis <jared@haiq.us>
2017-12-12 17:40:06 -0600

    refactor

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -1,11 +1,13 @@
 module IsbnVerifier (isbn) where
 
+import Data.Char (digitToInt)
+
 isbn :: String -> Bool
-isbn s = validDigits s && (isbnValue s) `mod` 11 == 0
+isbn s = validDigits s && isbnValue s `mod` 11 == 0
 
 validDigits :: String -> Bool
 validDigits s
-  | any (\digit -> digit == 'X') (take (length(s) - 1) s) = False
+  | 'X' `elem` take (length s - 1) s = False
   | otherwise = True
 
 isbnValue :: String -> Integer
@@ -15,21 +17,11 @@ mult :: (Integer, Integer) -> Integer
 mult (i, n) = i * n
 
 calculate :: [Integer] -> Integer
-calculate series = sum $ map mult (zip [10, 9..0] series)
+calculate series = sum $ zipWith (curry mult) [10, 9 .. 0] series
 
 keep :: String -> String -> String
 keep characters target = [ char | char <- target, char `elem` characters]
 
 change :: Char -> Integer
-change '0' = 0
-change '1' = 1
-change '2' = 2
-change '3' = 3
-change '4' = 4
-change '5' = 5
-change '6' = 6
-change '7' = 7
-change '8' = 8
-change '9' = 9
 change 'X' = 10
-change _ = 0
+change c = toInteger $ digitToInt c
